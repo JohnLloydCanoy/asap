ad712e2b8c1e2f8058ac038dc75b123496785df0
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-06-04 20:42:02 +0800

    feat: improve main

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,30 @@ import app.models
 
 load_dotenv()
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    print("⏳ Starting background worker...")
+    scheduler = BackgroundScheduler()
+
+    scheduler.add_job(process_pending_posts, 'interval', seconds=60)
+    scheduler.start()
+    
+    yield 
+
+
+    print("🛑 Shutting down background worker...")
+    scheduler.shutdown()
+
+
+app = FastAPI(title="ASAP API", lifespan=lifespan)
+
+
 origins = [origin.strip() for origin in os.getenv("CORS_ORIGINS", "").split(",") if origin]
+
+if not origins:
+    origins = ["http://localhost:3000", "http://127.0.0.1:3000"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,35 +46,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # What happens when the server STARTS:
-    print("⏳ Starting background worker...")
-    scheduler = BackgroundScheduler()
-    # Run the worker function every 60 seconds
-    scheduler.add_job(process_pending_posts, 'interval', seconds=60)
-    scheduler.start()
-    
-    yield 
-    
-
-    print("🛑 Shutting down background worker...")
-    scheduler.shutdown()
-
-
-app = FastAPI(title="ASAP API", lifespan=lifespan)
-
-
-
-
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(social_accounts.router)
 app.include_router(scheduled_posts.router)
-
-
 
 @app.get("/")
 def read_root():
